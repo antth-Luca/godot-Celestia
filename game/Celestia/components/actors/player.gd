7b88a6be2f6dc065f308545a9e388c333a44d8b6
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2025-03-27 10:32:32 -0300

    Movimento em 8 direções: Ok!
    * Alteração em configuração de renderizador de texturas para remover embaçado.

---
+++ game/Celestia/components/actors/player.gd
@@ -0,0 +1,60 @@
+extends CharacterBody2D
+
+const SPEED = 200.0
+const AIR_FRICTION = 0.5
+
+@export var jump_height := 68  # dobro da altura do personagem + 4
+@export var max_time_to_peak := 0.5
+
+var jump_velocity
+var gravity
+var fall_gravity
+var direction
+var is_jumping := false
+var is_hurted := false
+
+@onready var animation = $Sprite as AnimatedSprite2D
+
+
+func _ready():
+	jump_velocity = (jump_height * 2) / max_time_to_peak
+	gravity = (jump_height * 2) / pow(max_time_to_peak, 2)
+	fall_gravity = gravity * 2
+
+
+func _physics_process(_delta):
+	# Add the gravity.
+	# TODO
+
+	# Handle jump.
+	# TODO
+
+	# Get the input direction and handle the movement/deceleration.
+	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	# Normaliza a direção para evitar velocidade maior em diagonais
+	if direction != Vector2.ZERO:
+		direction = direction.normalized()
+
+	if direction.x != 0:
+		animation.scale.x = sign(direction.x)
+
+	velocity = direction * SPEED
+
+	_set_state()
+	move_and_slide()
+
+
+func _set_state():
+	var state = "idle"
+	
+	# if !is_on_floor():
+	# 	state = "jump"
+	# el
+	if direction != Vector2.ZERO:
+		state = "walk"
+
+	if is_hurted:
+		state = "hurt"
+
+	if animation.name != state:
+		animation.play(state)
