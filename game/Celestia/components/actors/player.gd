1d0ec127ee4b9d02e3a6157a6531d3848ea428ff
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2025-03-28 12:51:40 -0300

    pulos: Ok!

--- game/Celestia/components/actors/player.gd
+++ game/Celestia/components/actors/player.gd
@@ -1,10 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 60.0
-const AIR_FRICTION = 0.5
-
-@export var jump_height := 68  # dobro da altura do personagem + 4
-@export var max_time_to_peak := 0.5
+const SPEED = 80.0
 
 var jump_velocity
 var gravity
@@ -16,38 +12,42 @@ var is_hurted := false
 @onready var animation = $AnimationPlayer as AnimationPlayer
 
 
-func _ready():
-	jump_velocity = (jump_height * 2) / max_time_to_peak
-	gravity = (jump_height * 2) / pow(max_time_to_peak, 2)
-	fall_gravity = gravity * 2
-
-
 func _physics_process(_delta):
-	# Add the gravity.
-	# TODO
-
-	# Handle jump.
-	# TODO
+	if is_jumping: return
 
 	# Get the input direction and handle the movement/deceleration.
 	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
-
 	if direction != Vector2.ZERO:
 		velocity = direction * SPEED
 		if direction.x != 0: $Texture.scale.x = sign(direction.x)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
 
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and not is_jumping and is_valid_cell():
+		is_jumping = true
+		animation.play("jump")
+		var jump_tween = create_tween()
+		jump_tween.tween_property(
+			self,
+			"position",
+			global_position + (velocity * 0.8),
+			0.8
+		)
+		await jump_tween.finished
+		is_jumping = false
+
 	_set_state()
 	move_and_slide()
+
+
+func is_valid_cell():
+	return true
 
 
 func _set_state():
 	var state = "idle"
 	
-	# if !is_on_floor():
-	# 	state = "jump"
-	# el
 	if direction != Vector2.ZERO:
 		state = "walk"
 
