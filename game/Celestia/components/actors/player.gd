b02975430e37db69fb8ac21e92b910e7edfc3940
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2025-03-31 22:01:00 -0300

    Organização

--- game/Celestia/components/actors/player.gd
+++ game/Celestia/components/actors/player.gd
@@ -20,9 +20,8 @@ var is_jumping = false
 var is_hurted = false
 
 
-func _physics_process(_delta):
+func _physics_process(_delta) -> void:
 	if is_jumping: return
-
 	# Get the input direction and handle the movement/deceleration.
 	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
 	if direction != Vector2.ZERO:
@@ -30,11 +29,10 @@ func _physics_process(_delta):
 		if direction.x != 0: TEXTURE.scale.x = sign(direction.x)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
-
+	# Setando a posição do alvo do pulo e a posição do retângulo que prevê o pulo
 	set_target_cell()
-
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and not is_jumping:
+	if Input.is_action_just_pressed("ui_accept"):
 		is_jumping = true
 		ANIMATION.play("jump")
 		if is_valid_cell():
@@ -49,29 +47,26 @@ func _physics_process(_delta):
 		else:
 			await ANIMATION.animation_finished
 		is_jumping = false
-
+	# Setando estado e animação e continuando movimento
 	set_state()
 	move_and_slide()
 
 
-func set_target_cell():
+func set_target_cell() -> void:
 	target_cell = global_position + (velocity * 0.6)
 	RECT_TARGET_CELL.position = velocity * 0.6 + Vector2(-8, 8)
 
 
 func is_valid_cell() -> bool:
 	var world_cell = LAND_TILE_LAYER.local_to_map(LAND_TILE_LAYER.to_local(target_cell))
-	
 	var land_tile_id = LAND_TILE_LAYER.get_cell_source_id(world_cell)
 	var water_tile_id = WATER_TILE_LAYER.get_cell_source_id(world_cell)
-
 	if water_tile_id == -1 and land_tile_id != 2:
 		return true
-
 	return false
 
 
-func set_state():
+func set_state() -> void:
 	var state = "idle"
 	
 	if direction != Vector2.ZERO:
