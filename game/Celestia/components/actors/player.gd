3a59ef6a1d1b52cb03ee3d3189b2539cdffd64db
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2025-03-31 21:35:02 -0300

    Já não pula mais na água!!!!

--- game/Celestia/components/actors/player.gd
+++ game/Celestia/components/actors/player.gd
@@ -9,12 +9,13 @@ extends CharacterBody2D
 @onready var TEXTURE = $Texture as Sprite2D
 @onready var ANIMATION = $AnimationPlayer as AnimationPlayer
 @onready var WALL_COLLISIONS = $WallCollision as CollisionShape2D
-@onready var TARGET_CELL = $TargetCell as ColorRect
+@onready var RECT_TARGET_CELL = $TargetCell as ColorRect
 
 const SPEED = 80.0
 const AIR_FRICTION = 0.5
 
 var direction = Vector2.ZERO
+var target_cell = Vector2.ZERO
 var is_jumping = false
 var is_hurted = false
 
@@ -24,53 +25,50 @@ func _physics_process(_delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
-	# TODO: Remover depois
-	TARGET_CELL.position = Vector2.ZERO.lerp(direction * SPEED, AIR_FRICTION) * 0.6 + Vector2(-8, 8)
 	if direction != Vector2.ZERO:
 		velocity = velocity.lerp(direction * SPEED, AIR_FRICTION)
 		if direction.x != 0: TEXTURE.scale.x = sign(direction.x)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
 
+	set_target_cell()
+
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and not is_jumping and is_valid_cell():
+	if Input.is_action_just_pressed("ui_accept") and not is_jumping:
 		is_jumping = true
 		ANIMATION.play("jump")
-		var jump_tween = create_tween()
-		jump_tween.tween_property(
-			self,
-			"position",
-			global_position + (velocity * 0.6),
-			0.5
-		)
-		await jump_tween.finished
+		if is_valid_cell():
+			var jump_tween = create_tween()
+			jump_tween.tween_property(
+				self,
+				"position",
+				target_cell,
+				0.5
+			)
+			await jump_tween.finished
+		else:
+			await ANIMATION.animation_finished
 		is_jumping = false
 
 	set_state()
 	move_and_slide()
 
 
+func set_target_cell():
+	target_cell = global_position + (velocity * 0.6)
+	RECT_TARGET_CELL.position = velocity * 0.6 + Vector2(-8, 8)
+
+
 func is_valid_cell() -> bool:
-	return true
+	var world_cell = LAND_TILE_LAYER.local_to_map(LAND_TILE_LAYER.to_local(target_cell))
+	
+	var land_tile_id = LAND_TILE_LAYER.get_cell_source_id(world_cell)
+	var water_tile_id = WATER_TILE_LAYER.get_cell_source_id(world_cell)
 
-	var ID_WATER_LAYER = 0
-	var ID_LAND_LAYER = 1
-	var jump_target = global_position + (velocity * 0.6)
+	if land_tile_id != 2 and water_tile_id == -1:
+		return true
 
-	var cell_target = WATER_TILE_LAYER.local_to_map(WATER_TILE_LAYER.to_local(jump_target))
-	var water_id = WATER_TILE_LAYER.get_cell_source_id(cell_target)
-	if water_id:
-		print("É água")
-		return false
-
-	var land_id = LAND_TILE_LAYER.get_cell_source_id(cell_target)
-	var land_tile_data = LAND_TILE_LAYER.get_cell_tile_data(cell_target)
-	if land_tile_data and land_tile_data.get_collision_polygons_count(ID_LAND_LAYER) > 0:
-		print("É terra com colisão")
-		return false
-
-	print("Pode andar")
-	return true
+	return false
 
 
 func set_state():
