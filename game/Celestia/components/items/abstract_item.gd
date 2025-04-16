cd3b398f22f7ce664113ff06379fa6d8e5a46864
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2025-04-15 21:43:16 -0300

    Itens dropados no chão têm 1 segundo de delay entre tentativas de coleta

--- game/Celestia/components/items/abstract_item.gd
+++ game/Celestia/components/items/abstract_item.gd
@@ -1,6 +1,18 @@
 extends Sprite2D
 
 var stack: ItemStack
+var can_be_picked_up = false
+
+@onready var pickup_timer: Timer = Timer.new()
+
+
+func _ready():
+	# Configura o Timer e adiciona como filho do item
+	pickup_timer.wait_time = 1
+	pickup_timer.one_shot = true
+	pickup_timer.connect("timeout", Callable(self, "_on_pickup_timer_timeout"))
+	add_child(pickup_timer)
+	pickup_timer.start()
 
 
 func initialize(stack_param: ItemStack):
@@ -8,7 +20,13 @@ func initialize(stack_param: ItemStack):
 	texture = load("res://assets/textures/items/" + stack.item_class.icon_name + ".png")
 
 
+func _on_pickup_timer_timeout():
+	can_be_picked_up = true
+
+
 func _on_body_entered(body):
+	if !can_be_picked_up: return
+
 	if body.is_in_group("player"):
 		var inventory = body.get_node("UI/FullInventory/BackpackSlots")
 		if !inventory.is_full():
@@ -22,3 +40,6 @@ func _on_body_entered(body):
 			await follow_tween.finished
 			inventory.add_item_to_invent(stack)
 			queue_free()
+		else:
+			can_be_picked_up = false
+			pickup_timer.start()
