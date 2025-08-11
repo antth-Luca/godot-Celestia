0a882e22e1ea74280c062fc9e9feaf3b2fc137dd
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2025-08-11 12:53:14 -0300

    Com BUG

--- project/celestia/core/interface/hud/rotative_pocket/pseudoslot/PseudoSlot.gd
+++ project/celestia/core/interface/hud/rotative_pocket/pseudoslot/PseudoSlot.gd
@@ -3,11 +3,27 @@ extends Control
 @onready var itemSprite: Sprite2D = $ItemSprite
 @onready var itemAmount: Label = $ItemAmount
 
-
+# GODOT
 func _ready():
 	EventBus.client_inventory.connect('some_pocket_slot_has_updated', Callable(self, '_handle_pocket_slot_has_updated'))
+	clear_slot()
+
+# MAIN
+func render_slot(stack: ItemStack) -> void:
+	itemSprite.texture = load('res://assets/%s/textures/items/%s.png' % stack.get_item().get_splited_id())
+	itemSprite.visible = true
+	if stack.get_amount() > 1:
+		itemAmount.text = str(stack.get_amount())
+		itemAmount.visible = true
+	else:
+		itemAmount.visible = false
 
 
+func clear_slot() -> void:
+	itemSprite.visible = false
+	itemAmount.visible = false
+
+# HANDLRES
 func _handle_pocket_slot_has_updated(slot_index: int, stack: ItemStack) -> void:
 	if slot_index == get_index():
-		print('** O slot %s do bolso foi atualizado! **' % get_index())
+		render_slot(stack)
