f249346e6acc0e2b1fcb4a6692fcff27414edc6e
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2025-08-25 10:39:26 -0300

    Início de zumbi

--- project/celestia/core/entities/creatures/monsters/zombie/Zombie.gd
+++ project/celestia/core/entities/creatures/monsters/zombie/Zombie.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-class_name Player
+class_name Zombie
 
 @onready var TEXTURE: Sprite2D = $Texture
 @onready var ANIMATION: AnimationPlayer = $Animation
@@ -11,29 +11,26 @@ var is_hurted: bool = false
 # GODOT
 func _ready():
 	stats = PropertyManager.create_manager({
-		InitPropProviders.SURVIVOR_LEVEL: 1,
-		InitPropProviders.HEALTH: 100,
+		InitPropProviders.HEALTH: 80,
 		InitPropProviders.ARMOR: 0,
 		InitPropProviders.RESISTANCE: 0,
 		InitPropProviders.DAMAGE_REDUCTION: 0,
-		InitPropProviders.FORCE: 8,
+		InitPropProviders.FORCE: 16,
 		InitPropProviders.PENETRATION: 0,
 		InitPropProviders.DEFENSE_REDUCTION: 0,
 		InitPropProviders.CRITICAL_STRIKE: 0,
-		InitPropProviders.LIFE_STEAL: 0,
+		InitPropProviders.LIFE_STEAL: 3,
 		InitPropProviders.MANA: 50,
 		InitPropProviders.RANGE: 1,
 		InitPropProviders.USE_SPEED: 1,
-		InitPropProviders.MOVE_SPEED: 80,
+		InitPropProviders.MOVE_SPEED: 40,
 		InitPropProviders.COOLDOWN_REDUCTION: 0
 	})
-
-	EventBus.client_player.connect('level_up', Callable(self, '_on_surv_level_up'))
 
 
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
-	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
+	direction = Vector2(0, 0)
 	# Get the input direction and handle the movement/deceleration.
 	var stats_move_speed = stats.get_property(InitPropProviders.MOVE_SPEED).get_move_speed()
 	if direction != Vector2.ZERO:
@@ -46,10 +43,6 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 # GETTERS AND SETTERS
-# Nodes
-func get_ui():
-	return get_node('WorldUI')
-
 # State
 func set_state() -> void:
 	var state = "idle"
@@ -59,12 +52,3 @@ func set_state() -> void:
 		state = "hurt"
 	if ANIMATION.current_animation != state:
 		ANIMATION.play(state)
-
-# HANDLERS
-func _on_surv_level_up() -> void:
-	var health_prov = stats.get_property(InitPropProviders.HEALTH)
-	health_prov.add_max_health(10)
-	health_prov.add_health(10)
-	stats.get_property(InitPropProviders.FORCE).add_force(2.5)
-	stats.get_property(InitPropProviders.RESISTANCE).add_resistance(0.5)
-	stats.get_property(InitPropProviders.PENETRATION).add_penetration(0.2)
