47cb352a15b2f692d81867d7ec946d7528672f2b
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2025-09-09 21:16:39 -0300

    Efeito de delay na decaída das barras de stats na HUD

--- project/celestia/core/interface/hud/stats_bar/StatsBar.gd
+++ project/celestia/core/interface/hud/stats_bar/StatsBar.gd
@@ -1,22 +1,72 @@
 extends Node2D
 class_name StatsBar
 
+const default_change_time: float = .4
+
 @onready var health_bar: ProgressBar = $HealthBar
+@onready var prev_health_bar: ProgressBar = $HealthBar/PrevHealthBar
+@onready var health_timer: Timer = $HealthBar/HealthTimer
+
 @onready var mana_bar: ProgressBar = $ManaBar
+@onready var prev_mana_bar: ProgressBar = $ManaBar/PrevManaBar
+@onready var mana_timer: Timer = $ManaBar/ManaTimer
+
 @onready var hungry_bar: ProgressBar = $HungryBar
+@onready var prev_hungry_bar: ProgressBar = $HungryBar/PrevHungryBar
+@onready var hungry_timer: Timer = $HungryBar/HungryTimer
 
+# GODOT
+func _ready():
+	health_timer.wait_time = default_change_time
+	mana_timer.wait_time = default_change_time
+	hungry_timer.wait_time = default_change_time
 
+# EXTERNAL SIGNALS
+# Health
 func _on_max_health_changed(newMaxHP: float):
 	health_bar.max_value = newMaxHP
+	prev_health_bar.max_value = newMaxHP
 
 
 func _on_health_changed(newHP: float):
+	var prev_health: float = health_bar.value
 	health_bar.value = newHP
+	if newHP < prev_health:
+		health_timer.start()
+	else:
+		prev_health_bar.value = newHP
 
-
+# Mana
 func _on_max_mana_changed(newMaxMP: float):
 	mana_bar.max_value = newMaxMP
+	prev_mana_bar.max_value = newMaxMP
 
 
 func _on_mana_changed(newMP: float):
+	var prev_mana: float = mana_bar.value
 	mana_bar.value = newMP
+	if newMP < prev_mana:
+		mana_timer.start()
+	else:
+		prev_mana_bar.value = newMP
+
+# Hungry
+func _on_hungry_changed(newHungry: float):
+	var prev_hungry: float = hungry_bar.value
+	hungry_bar.value = newHungry
+	if newHungry < prev_hungry:
+		hungry_timer.start()
+	else:
+		prev_hungry_bar.value = newHungry
+
+# INTERNAL SIGNALS
+func _on_health_timer_timeout():
+	prev_health_bar.value = health_bar.value
+
+
+func _on_mana_timer_timeout():
+	prev_mana_bar.value = mana_bar.value
+
+
+func _on_hungry_timer_timeout():
+	prev_hungry_bar.value = hungry_bar.value
