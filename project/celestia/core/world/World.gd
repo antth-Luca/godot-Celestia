46dd65838c17001f01b53970420d7181fa5b536f
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2025-09-17 10:42:08 -0300

    Representação visual de dia e noite: Ok
    [ ! ] Necessita acertar a velocidade de dia e noite e melhorar a definição de tempo em ticks.

--- project/celestia/core/world/World.gd
+++ project/celestia/core/world/World.gd
@@ -1,8 +1,13 @@
 extends Node2D
 
-var config := {}
+@onready var natural_light: CanvasModulate = $NaturalLight
+@onready var day_night_cycle_gradient: GradientTexture1D = load('res://assets/celestia/particles/daynightcycle-gradient-texture.tres')
 
-func _ready():
+var config := {}
+var time: float = 0
+
+# GODOT
+func _ready() -> void:
 	# World config
 	var config_file = FileAccess.open('res://saves/MyWorld/config/world_config.json', FileAccess.READ)
 	if config_file:
@@ -16,12 +21,18 @@ func _ready():
 	player.global_position = Vector2(169, 104)
 	player.ESSENCE_COUNTER = config.get('max_resilient_essence')
 	# TESTS CODE
-	# Zombie
+	# Spawn Zombie
 	var zombie: Zombie = InitEntities.ZOMBIE.get_listed().instantiate()
 	get_tree().current_scene.add_child(zombie)
 	zombie.global_position = Vector2(487, 117)
-	# Sword
+	# Spawn Sword
 	DroppedItemUtils.drop_item_in_position(
 		ItemStack.new(InitWeapons.IRON_SWORD.get_registered(), 1),
 		Vector2(208, 133)
 	)
+
+
+func _process(delta: float) -> void:
+	time += delta
+	var color_time_value = (sin(time - PI / 2) + 1.0) / 2.0
+	natural_light.color = day_night_cycle_gradient.gradient.sample(color_time_value)
