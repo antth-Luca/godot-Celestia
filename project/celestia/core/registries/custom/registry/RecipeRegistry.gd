77730c6fd3b1e7c81daceb71e685fcad894bcca8
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2025-09-25 12:51:30 -0300

    Add RecipeRegistry customizado para receitas

--- project/celestia/core/registries/custom/registry/RecipeRegistry.gd
+++ project/celestia/core/registries/custom/registry/RecipeRegistry.gd
@@ -9,3 +9,17 @@ var _per_ingredient: Dictionary[DeferredHolder, Array] = {}
 
 func register(id: ResourceLocation, entry: Callable) -> void:
 	super.register(id, entry)
+	# Get recipe
+	var recipe = entry.call()
+	if not recipe:
+		push_error('RecipeRegistry: Recipe %s is null' % id.get_string())
+		return
+	# Per workstation
+	if not _per_workstation.has(recipe.workstation):
+		_per_workstation[recipe.workstation] = []
+	_per_workstation[recipe.workstation].append(id)
+	# Per ingredient
+	for ing in recipe.get_ingredients():
+		if not _per_ingredient.has(ing):
+			_per_ingredient[ing] = []
+		_per_ingredient[ing].append(id)
