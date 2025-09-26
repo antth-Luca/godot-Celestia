44b05d8711b08c51f894006355cca43f6a11077a
antth-Luca <123425103+antth-Luca@users.noreply.github.com>
2025-09-26 14:46:04 -0300

    Realocação de "Recipes" e melhoria nos botões de aba do MyPanel

--- project/celestia/core/interface/tab_inventory/MyPanel.gd
+++ project/celestia/core/interface/tab_inventory/MyPanel.gd
@@ -4,18 +4,30 @@ class_name MyPanel
 @onready var inventory_tab: InventoryManager = $InventoryTab
 @onready var stats_tab: StatsManager = $StatsTab
 
+var selected_tab: int
+var tab_buttons: Array[TextureButton] = []
+
 # GODOT
 func _ready() -> void:
+	# Variables
+	for btn in get_node('Background/TabButtons').get_children():
+		tab_buttons.append(btn)
+	# Node control
 	visible = false
 	_show_inventory()
+	selected_tab = 0
 
 
 func _input(event):
 	if self.visible:
-		if event.is_action_pressed("ui_tab_1"):
-			_on_inventory_button_pressed()
-		elif event.is_action_pressed("ui_tab_2"):
-			_on_stats_button_pressed()
+		if event.is_action_pressed("ui_left"):
+			selected_tab += 1
+			if selected_tab > tab_buttons.size() -1: selected_tab = 0
+			tab_buttons[selected_tab].emit_signal('pressed')
+		elif event.is_action_pressed("ui_right"):
+			selected_tab -= 1
+			if selected_tab < 0: selected_tab = tab_buttons.size() - 1
+			tab_buttons[selected_tab].emit_signal('pressed')
 	else:
 			inventory_tab._on_inventory_closed()
 
