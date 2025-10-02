55d843c78f9b2491b7b7b66a5c35c01fea111f13
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2025-10-01 21:41:51 -0300

    Reconstrução de Recipes e seus tipos pt.2

--- project/celestia/core/items/recipes/WithReturnRecipe.gd
+++ project/celestia/core/items/recipes/WithReturnRecipe.gd
@@ -34,16 +34,14 @@ func set_result(result: Ingredient) -> void:
 
 # GETTERS AND SETTERS
 func get_returns() -> Array[ItemStack]:
+	if _returns.size() != _damage_p_return.size(): return []
 	var stacks_return: Array[ItemStack] = []
 	for c in _returns.size():
-		var stack := ItemStack.new(_returns[c].front().get_registered(), _returns[c].back())
+		var stack := ItemStack.new(_returns[c].item_holder.get_registered(), _returns[c].amount)
 		stack.item.consome_durability(_damage_p_return[c], null)
 	return stacks_return
 
 
-func add_return(return_item: DeferredHolder, return_amount: int, return_damage: int) -> void:
-	_returns.append([
-		return_item,
-		return_amount
-	])
+func add_return(item_holder: DeferredHolder, amount: int, return_damage: int) -> void:
+	_returns.append(Ingredient.new(item_holder, amount))
 	_damage_p_return.append(return_damage)
