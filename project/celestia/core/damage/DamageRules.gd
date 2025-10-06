e766dec83ef14fa5956c1671f285c7f865fff775
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2025-10-06 10:19:12 -0300

    Tentando alterar o dano à estruturas

--- project/celestia/core/damage/DamageRules.gd
+++ project/celestia/core/damage/DamageRules.gd
@@ -5,7 +5,8 @@ const DAMAGE_RULES = {
 	EntityData.FACTION_MASK.PLAYER: 
 		EntityData.FACTION_MASK.BOSS | 
 		EntityData.FACTION_MASK.MONSTER |
-		EntityData.FACTION_MASK.ANIMAL,
+		EntityData.FACTION_MASK.ANIMAL |
+		EntityData.FACTION_MASK.STRUCTURE,
 	EntityData.FACTION_MASK.BOSS:
 		EntityData.FACTION_MASK.PLAYER,
 	EntityData.FACTION_MASK.MONSTER:
@@ -13,13 +14,30 @@ const DAMAGE_RULES = {
 		EntityData.FACTION_MASK.MONSTER,
 	EntityData.FACTION_MASK.ANIMAL:
 		EntityData.FACTION_MASK.PLAYER |
-		EntityData.FACTION_MASK.ANIMAL
+		EntityData.FACTION_MASK.ANIMAL,
+	EntityData.FACTION_MASK.STRUCTURE: 0
 }
 
 static func can_damage(hit: HitData, target: EntityData) -> bool:
+	if (DAMAGE_RULES.get(hit.attacker.entity_data.faction, 0) & EntityData.FACTION_MASK.STRUCTURE):
+		return can_damage_structure(hit, target as StructureData)
 	if (
 		hit.attacker == target or
 		target.is_dead or
 		target.is_invincible
 	): return false
 	return (DAMAGE_RULES.get(hit.attacker.entity_data.faction, 0) & target.faction) != 0
+
+
+static func can_damage_structure(hit: HitData, target: StructureData) -> bool:
+	if not hit.tool: return false
+	var tool_is_compatible: bool = false
+	for comp_tool in target.compatible_tools:
+		tool_is_compatible = hit.tool.is_class(comp_tool)
+		if tool_is_compatible: break
+	if not tool_is_compatible: return false
+	var material_is_compatible: bool = false
+	for comp_mat in target.compatible_materials:
+		material_is_compatible = hit.tool.material.is_class(comp_mat)
+		if material_is_compatible: break
+	return material_is_compatible
