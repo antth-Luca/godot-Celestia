4e8267f057470cf0b19e208e239c40179eef1625
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2025-10-07 09:45:24 -0300

    Adição de offset ao spawnar estrutura nos pés do Player

--- project/celestia/core/structures/utils/StructuresUtils.gd
+++ project/celestia/core/structures/utils/StructuresUtils.gd
@@ -7,5 +7,10 @@ func spawn_structure_in_position(structure: BaseStructure, pos: Vector2) -> void
 
 
 func spawn_structure_entity_foot(structure: BaseStructure, entity: LivingEntity) -> void:
-	var at_pos = entity.global_position  # TODO: Adicionar correção de pos para ter um offset.
+	var dir: Vector2
+	if entity.direction != Vector2.ZERO:
+		dir = entity.direction.normalized() * -1
+	else:
+		dir = Vector2(-1, 0) if entity.TEXTURE.flip_h else Vector2(1, 0)
+	var at_pos: Vector2 = entity.global_position + dir * 20
 	spawn_structure_in_position(structure, at_pos)
