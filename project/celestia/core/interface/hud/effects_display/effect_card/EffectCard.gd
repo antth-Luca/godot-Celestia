75908a05fbbe6d3547dfc533d3e47483c4faf728
antth-Luca <123425103+antth-Luca@users.noreply.github.com>
2025-10-14 16:23:08 -0300

    Adição do EffectCard

---
+++ project/celestia/core/interface/hud/effects_display/effect_card/EffectCard.gd
@@ -0,0 +1,6 @@
+extends Node2D
+class_name EffectCard
+
+@onready var TEXTURE: Sprite2D = $Texture
+@onready var label_amplifier: Label = $EffectAmplifier
+@onready var progress_bar: ProgressBar = $ProgressBar
