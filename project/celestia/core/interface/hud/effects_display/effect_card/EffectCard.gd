da5544ccf8fac0435da64b157face3c3f280eb76
antth-Luca <123425103+antth-Luca@users.noreply.github.com>
2025-10-14 16:49:46 -0300

    ...

--- project/celestia/core/interface/hud/effects_display/effect_card/EffectCard.gd
+++ project/celestia/core/interface/hud/effects_display/effect_card/EffectCard.gd
@@ -4,3 +4,21 @@ class_name EffectCard
 @onready var TEXTURE: Sprite2D = $Texture
 @onready var label_amplifier: Label = $EffectAmplifier
 @onready var progress_bar: ProgressBar = $ProgressBar
+
+var remaining_effect_tween: Tween
+
+# MAIN
+func initialize(effect: BaseEffect) -> void:
+	TEXTURE.texture = load(Celestia.EFFECT_SPRITE_PATH % effect.id.get_splited())
+	_on_effect_updated(effect)
+
+
+func _on_effect_updated(effect: BaseEffect) -> void:
+	# Label
+	if effect.amplifier > 1:
+		label_amplifier.text = str(effect.amplifier)
+		label_amplifier.visible = true
+	else:
+		label_amplifier.visible = false
+	# Progress bar
+	# TODO: Completar aqui!
