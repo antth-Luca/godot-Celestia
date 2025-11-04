91811a34cef12534fd1b90c76fc00a00c2efdb4d
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2025-11-04 12:50:54 -0300

    Início ArcaneLinesRelic passive

--- project/celestia/core/items/relic/custom/ArcaneLinesRelic.gd
+++ project/celestia/core/items/relic/custom/ArcaneLinesRelic.gd
@@ -2,12 +2,16 @@ extends BaseRelic
 class_name ArcaneLinesRelic
 
 const BUFF_MANA: float = 15
+const BUFF_DAMAGE_PERCENTAGE: float = .15
+
+var last_max_mana: float
 
 # SUPER
 # Handlers
 func on_equip(slot: BaseSlot, player: Player) -> void:
 	if slot.slot_type != BaseSlot.Type.RELIC: return
 	var player_mp_prop: ManaProperty = player.entity_data.stats.get_property(InitPropProviders.MANA)
+	player_mp_prop.connect('max_mana_changed', Callable(self, '_on_max_mana_changed'))
 	player_mp_prop.add_max_mana(BUFF_MANA)
 	player_mp_prop.add_mana(BUFF_MANA)
 
@@ -17,6 +21,16 @@ func on_unequip(slot: BaseSlot, player: Player):
 	var player_mp_prop: ManaProperty = player.entity_data.stats.get_property(InitPropProviders.MANA)
 	player_mp_prop.sub_mana(BUFF_MANA)
 	player_mp_prop.sub_max_mana(BUFF_MANA)
+	player_mp_prop.disconnect('max_mana_changed', Callable(self, '_on_max_mana_changed'))
 
-# Hooks
-# TODO: Add passiva.
+# HANDLERS
+func _on_max_mana_changed(newMaxMP: float) -> void:
+	if last_max_mana == newMaxMP: return
+	if newMaxMP > last_max_mana:
+		var difference: float = newMaxMP - last_max_mana
+		var _a = difference * BUFF_DAMAGE_PERCENTAGE
+		last_max_mana = newMaxMP
+		return
+	# newMaxMP < last_max_mana
+	pass
+	last_max_mana = newMaxMP
